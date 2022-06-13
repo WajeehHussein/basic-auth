cd0cf9a2ec7161cbbb83ab632d283299d2a20ba3
WajeehHussein <wajehessam3@gmail.com>
2022-06-13 23:29:04 +0300

    heroku

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 require('dotenv').config();
-let PORT = process.env.PORT || 3000
+let PORT = process.env.PORT
 
 const server = require('./src/server')
 const { sequelize } = require('./src/models/index')
 
 
 sequelize.sync().then(() => {
-    server.start(3000, () => console.log('server up'));
+    server.start(PORT, () => console.log('server up'));
 }).catch(e => {
     console.error('Could not start server', e.message);
 });
