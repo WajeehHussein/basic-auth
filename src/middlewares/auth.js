65720212e5609c0b1a8795c0a5522ad5d8741093
WajeehHussein <wajehessam3@gmail.com>
2022-06-13 16:55:50 +0300

    end

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,13 +1,24 @@
 'use strict';
 const base64 = require('base-64');
 const bcrypt = require('bcrypt');
-const { Users } = require('../models/index')
 
 
-async function auth(req, res, next) {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    const record = await Users.create(req.body);
+module.exports = (Users) => async (req, res, next) => {
+    console.log(Users);
+    let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'sdkjdsljd=']
+    let encodedString = basicHeaderParts.pop();  // sdkjdsljd=
+    let decodedString = base64.decode(encodedString); // "username:password"
+    let [username, password] = decodedString.split(':'); // username, password
+
+    try {
+        const user = await Users.findOne({ where: { username: username } });
+        const valid = await bcrypt.compare(password, user.password);
+        if (valid) {
+            req.user = user;
+        }
+        else {
+            throw new Error('Invalid User');
+        }
+    } catch (error) { res.status(403).send('Invalid Login'); }
     next();
 }
-
-module.exports = auth
